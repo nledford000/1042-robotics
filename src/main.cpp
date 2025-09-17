6a1a3bb94607cf9519170e80363cf568d472633f
Noah Ledford <nledford000@student.rcschools.net>
2025-09-16 19:26:03 -0500

    Initial commit - Team 1042 VEX project

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       C:\Users\student                                          */
+/*    Created:      Tue Sep 16 2025                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+    
+}
