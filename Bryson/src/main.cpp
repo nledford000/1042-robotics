18718e3606ff55a90d59f78c5c36e3b68ce0c1f2
bryson <brysonparker2012@gmail.com>
2025-09-20 13:03:01 -0500

    homework

--- Bryson/src/main.cpp
+++ Bryson/src/main.cpp
@@ -23,6 +23,9 @@ int main() {
 int Holding =0;
 int Stickpos = Controller1.Axis4.position();
 
+bool Converor = false;
+bool Realese = false;
+
 while(true){
  if(Stickpos > -5 && Stickpos < 5){
   Stickpos = 0;   
@@ -52,6 +55,30 @@ else if(Controller1.ButtonR2.pressing()){
   Controller1.Screen.newLine();
   Brain.Screen.print("%d", Holding);
   Controller1.Screen.print("%d", Holding);
+}
+
+if(Controller1.ButtonX.pressing()){
+    Converor = !Converor; // toggle the motor state
+}
+  if(Converor == true){
+    ConvMotor.spin(forward, 100,pct);
+}
+  else{
+  ConvMotor.stop();
+}
+    wait(20, msec);
+
+    if(Controller1.ButtonB.pressing()){
+    Realese = !Realese; 
+     if(Realese = false){
+  Servo2.setPosition(90, pct);
+}
+ else if(Realese = true){
+  Servo2.setPosition(180, pct);
+}
+
+ 
+}
 }
  
 }
