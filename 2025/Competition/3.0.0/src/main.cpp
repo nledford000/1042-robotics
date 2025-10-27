25336a6bd3a80861c7fc631e4d3818372e6f73ba
Noah Ledford <nledford000@student.rcschools.net>
2025-10-26 21:12:22 -0500

    Improve arm control and add manual actions
    Refactor arm control logic and add manual control functions.

--- 2025/Competition/3.0.0/src/main.cpp
+++ 2025/Competition/3.0.0/src/main.cpp
@@ -1,22 +1,8 @@
 #include "vex.h"
 
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// Controller1          controller                    
-// ArmMotor             motor         3               
-// NeuralMotor          motor         4               
-// NeuralPot            pot           A               
-// ServoOC              servo         B               
-// ServoPush            servo         C               
-// LeftMotor            motor         1               
-// RightMotor           motor         2               
-// ArmPot               pot           D               
-// ---- END VEXCODE CONFIGURED DEVICES ----
-
 using namespace vex;
 
-// Constants
+//CONSTANTS
 const int driveDeadband = 15;
 const int magOpen = 100;
 const int magClosed = 0;
@@ -26,117 +12,122 @@ const int panelBack = 0;
 const double armLow = 225;
 const double armMid = 125;
 const double armHigh = 75;
-
 const double armSpeed = 60;
-const double armTolerance = 15;
+const double armTolerance = 5;
 
-// State variables
+//STATE VARIABLES
 int armState = 0;
-double armTarget = 250;
+double armTarget = armLow;
 bool armAutoMove = false;
 bool magToggle = false;
 bool panelToggle = false;
 bool neuralToggle = false;
 
-// Utility
 double clamp(double value, double minVal, double maxVal) {
-return value < minVal ? minVal : (value > maxVal ? maxVal : value);
+  return value < minVal ? minVal : (value > maxVal ? maxVal : value);
 }
 
-// Arm control thread
-int armControlThread() {
-while (true) {
-if (Controller1.ButtonR2.pressing()) {
-ArmMotor.spin(forward, armSpeed, pct);
-armAutoMove = false;
-} else if (Controller1.ButtonR1.pressing()) {
-ArmMotor.spin(reverse, armSpeed, pct);
-armAutoMove = false;
-} else if (armAutoMove) {
-double current = ArmPot.angle(degrees);
-if (fabs(armTarget - current) > armTolerance) {
-ArmMotor.spin(current < armTarget ? forward : reverse, armSpeed, pct);
-} else {
-ArmMotor.stop(hold);
-armAutoMove = false;
-}
-} else {
-ArmMotor.stop(hold);
-}
-wait(20, msec);
-}
-return 0;
-}
-
-// Actions
 void toggleMagAction() {
-magToggle = !magToggle;
-ServoOC.setPosition(magToggle ? magOpen : magClosed, degrees);
+  magToggle = !magToggle;
+  ServoOC.setPosition(magToggle ? magOpen : magClosed, degrees);
 }
 
 void togglePanelAction() {
-panelToggle = !panelToggle;
-ServoPush.setPosition(panelToggle ? panelForward : panelBack, degrees);
-}
-
-void cycleArmPosition() {
-armState = (armState + 1) % 3;
-armTarget = (armState == 0) ? armLow : (armState == 1) ? armMid : armHigh;
-armAutoMove = true;
+  panelToggle = !panelToggle;
+  ServoPush.setPosition(panelToggle ? panelForward : panelBack, degrees);
 }
 
 void toggleNeuralMotor() {
-neuralToggle = !neuralToggle;
-if (neuralToggle) {
-NeuralMotor.spin(forward, 100, pct);
-} else {
-NeuralMotor.stop(coast);
-}
+  neuralToggle = !neuralToggle;
+  if (neuralToggle)
+    NeuralMotor.spin(forward, 100, pct);
+  else
+    NeuralMotor.stop(coast);
 }
 
-// Button checks
+void cycleArmPosition() {
+  armState++;
+  if (armState > 3) armState = 1;
+
+  if (armState == 1) armTarget = armLow;
+  else if (armState == 2) armTarget = armMid;
+  else if (armState == 3) armTarget = armHigh;
+
+  armAutoMove = true;
+}
+
 bool buttonYPressed() { return Controller1.ButtonY.pressing(); }
 bool buttonAPressed() { return Controller1.ButtonA.pressing(); }
 bool buttonXPressed() { return Controller1.ButtonX.pressing(); }
 bool buttonBPressed() { return Controller1.ButtonB.pressing(); }
+bool buttonR1Pressed() { return Controller1.ButtonR1.pressing(); }
+bool buttonR2Pressed() { return Controller1.ButtonR2.pressing(); }
 
-// Action handler
 struct Action {
-bool lastState;
-void (*onPress)();
-bool (*isPressed)();
+  bool lastState;
+  void (*onPress)();
+  bool (*isPressed)();
 
-void update() {
-bool now = isPressed();
-if (now && !lastState && onPress) onPress();
-lastState = now;
-}
+  void update() {
+    bool now = isPressed();
+    if (now && !lastState && onPress) onPress();
+    lastState = now;
+  }
 };
+
+int armAutoThread() {
+  while (true) {
+    if (armAutoMove) {
+      double current = ArmPot.value(deg);
+      double error = armTarget - current;
+
+      if (fabs(error) > armTolerance) {
+        ArmMotor.spin(error > 0 ? reverse : forward, armSpeed, pct);
+      } else {
+        ArmMotor.stop(hold);
+        armAutoMove = false;
+      }
+    } else {
+      ArmMotor.stop(hold);
+    }
+  }
+  return 0;
+}
+
+void armUpManual() {
+  ArmMotor.spin(reverse, armSpeed, pct);
+  armAutoMove = false;
+}
+
+void armDownManual() {
+  ArmMotor.spin(forward, armSpeed, pct);
+  armAutoMove = false; 
+}
 
 int main() {
-vexcodeInit();
-thread armThread(armControlThread);
+  vexcodeInit();
 
-Action actions[] = {
-{false, toggleMagAction, buttonYPressed},
-{false, togglePanelAction, buttonAPressed},
-{false, toggleNeuralMotor, buttonXPressed},
-{false, cycleArmPosition, buttonBPressed}
-};
+  thread armThread(armAutoThread);
 
-while (true) {
-int leftSpeed = Controller1.Axis3.position();
-int rightSpeed = Controller1.Axis2.position();
+  Action actions[] = {
+    {false, toggleMagAction, buttonYPressed},
+    {false, togglePanelAction, buttonAPressed},
+    {false, toggleNeuralMotor, buttonXPressed},
+    {false, cycleArmPosition, buttonBPressed},
+    {false, armUpManual, buttonR1Pressed},
+    {false, armDownManual, buttonR2Pressed}
+  };
 
-if (abs(leftSpeed) < driveDeadband) leftSpeed = 0;
-if (abs(rightSpeed) < driveDeadband) rightSpeed = 0;
+  while (true) {
+    int leftSpeed = Controller1.Axis3.position();
+    int rightSpeed = Controller1.Axis2.position();
 
-LeftMotor.spin(forward, leftSpeed, pct);
-RightMotor.spin(forward, rightSpeed, pct);
+    if (abs(leftSpeed) < driveDeadband) leftSpeed = 0;
+    if (abs(rightSpeed) < driveDeadband) rightSpeed = 0;
 
-for (int i = 0; i < 4; i++) actions[i].update();
+    LeftMotor.spin(forward, leftSpeed, pct);
+    RightMotor.spin(forward, rightSpeed, pct);
 
-wait(20, msec);
+    for (int i = 0; i < 6; i++) actions[i].update();
+  }
 }
-}
-
