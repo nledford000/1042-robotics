f567248c4335d4e63d1732049823a36b709f3ada
Noah Ledford <nledford000@student.rcschools.net>
2025-10-27 18:17:53 -0500

    Update main.cpp

--- 2025/Competition/3.0.0/src/main.cpp
+++ 2025/Competition/3.0.0/src/main.cpp
@@ -2,62 +2,66 @@
 
 using namespace vex;
 
-//CONSTANTS
 const int driveDeadband = 15;
+const double armLow = 223; 
+const double armMid = 60;
+const double armHigh = 1;
+const double armUpSpeed = 100;
+const double armDownSpeed = 60;
+const double armTolerance = 5;
 const int magOpen = 100;
 const int magClosed = 0;
-const int panelForward = 80;
-const int panelBack = 0;
+const double neuralPos1 = 0;//0.67, 67, 151, 250
+const double neuralPos2 = 67;
+const double neuralPos3 = 151;
+const double neuralPos4 = 250;
+const double neuralUpSpeed = 80;
+const double neuralDownSpeed = 60;
+const double neuralTolerance = 5;
 
-const double armLow = 225;
-const double armMid = 125;
-const double armHigh = 75;
-const double armSpeed = 60;
-const double armTolerance = 5;
-
-//STATE VARIABLES
-int armState = 0;
+int armState = 1;
 double armTarget = armLow;
 bool armAutoMove = false;
 bool magToggle = false;
-bool panelToggle = false;
-bool neuralToggle = false;
-
-double clamp(double value, double minVal, double maxVal) {
-  return value < minVal ? minVal : (value > maxVal ? maxVal : value);
-}
+int neuralState = 1;
+double neuralTarget = neuralPos1;
+bool neuralAutoMove = false;
 
 void toggleMagAction() {
   magToggle = !magToggle;
   ServoOC.setPosition(magToggle ? magOpen : magClosed, degrees);
 }
 
-void togglePanelAction() {
-  panelToggle = !panelToggle;
-  ServoPush.setPosition(panelToggle ? panelForward : panelBack, degrees);
-}
-
-void toggleNeuralMotor() {
-  neuralToggle = !neuralToggle;
-  if (neuralToggle)
-    NeuralMotor.spin(forward, 100, pct);
-  else
-    NeuralMotor.stop(coast);
-}
-
 void cycleArmPosition() {
   armState++;
   if (armState > 3) armState = 1;
-
   if (armState == 1) armTarget = armLow;
   else if (armState == 2) armTarget = armMid;
   else if (armState == 3) armTarget = armHigh;
-
   armAutoMove = true;
 }
 
+void cycleNeuralPosition() {
+  neuralState++;
+  if (neuralState > 4) neuralState = 1;
+  if (neuralState == 1) neuralTarget = neuralPos1;
+  else if (neuralState == 2) neuralTarget = neuralPos2;
+  else if (neuralState == 3) neuralTarget = neuralPos3;
+  else if (neuralState == 4) neuralTarget = neuralPos4;
+  neuralAutoMove = true;
+}
+
+void armUpManual() {
+  ArmMotor.spin(forward, armUpSpeed, pct);
+  armAutoMove = false;
+}
+
+void armDownManual() {
+  ArmMotor.spin(reverse, armDownSpeed, pct);
+  armAutoMove = false;
+}
+
 bool buttonYPressed() { return Controller1.ButtonY.pressing(); }
-bool buttonAPressed() { return Controller1.ButtonA.pressing(); }
 bool buttonXPressed() { return Controller1.ButtonX.pressing(); }
 bool buttonBPressed() { return Controller1.ButtonB.pressing(); }
 bool buttonR1Pressed() { return Controller1.ButtonR1.pressing(); }
@@ -67,10 +71,10 @@ struct Action {
   bool lastState;
   void (*onPress)();
   bool (*isPressed)();
-
   void update() {
     bool now = isPressed();
-    if (now && !lastState && onPress) onPress();
+    if (now && !lastState && onPress)
+      onPress();
     lastState = now;
   }
 };
@@ -80,54 +84,61 @@ int armAutoThread() {
     if (armAutoMove) {
       double current = ArmPot.value(deg);
       double error = armTarget - current;
-
       if (fabs(error) > armTolerance) {
-        ArmMotor.spin(error > 0 ? reverse : forward, armSpeed, pct);
+        ArmMotor.spin(error > 0 ? reverse : forward,
+                      (error > 0 ? armDownSpeed : armUpSpeed), pct);
       } else {
         ArmMotor.stop(hold);
         armAutoMove = false;
       }
-    } else {
+    } else if (!Controller1.ButtonR1.pressing() && !Controller1.ButtonR2.pressing()) {
       ArmMotor.stop(hold);
     }
+    this_thread::sleep_for(20);
   }
   return 0;
 }
 
-void armUpManual() {
-  ArmMotor.spin(reverse, armSpeed, pct);
-  armAutoMove = false;
-}
-
-void armDownManual() {
-  ArmMotor.spin(forward, armSpeed, pct);
-  armAutoMove = false; 
+int neuralAutoThread() {
+  while (true) {
+    if (neuralAutoMove) {
+      double current = NeuralPot.value(deg);
+      double error = neuralTarget - current;
+      if (fabs(error) > neuralTolerance) {
+        NeuralMotor.spin(error > 0 ? reverse : forward,
+                         (error > 0 ? neuralDownSpeed : neuralUpSpeed), pct);
+      } else {
+        NeuralMotor.stop(hold);
+        neuralAutoMove = false;
+      }
+    } else {
+      NeuralMotor.stop(hold);
+    }
+    this_thread::sleep_for(20);
+  }
+  return 0;
 }
 
 int main() {
   vexcodeInit();
-
   thread armThread(armAutoThread);
-
+  thread neuralThread(neuralAutoThread);
   Action actions[] = {
     {false, toggleMagAction, buttonYPressed},
-    {false, togglePanelAction, buttonAPressed},
-    {false, toggleNeuralMotor, buttonXPressed},
+    {false, cycleNeuralPosition, buttonXPressed},
     {false, cycleArmPosition, buttonBPressed},
-    {false, armUpManual, buttonR1Pressed},
-    {false, armDownManual, buttonR2Pressed}
+    {false, armDownManual, buttonR2Pressed},
+    {false, armUpManual, buttonR1Pressed}
   };
-
   while (true) {
     int leftSpeed = Controller1.Axis3.position();
     int rightSpeed = Controller1.Axis2.position();
-
     if (abs(leftSpeed) < driveDeadband) leftSpeed = 0;
     if (abs(rightSpeed) < driveDeadband) rightSpeed = 0;
-
     LeftMotor.spin(forward, leftSpeed, pct);
     RightMotor.spin(forward, rightSpeed, pct);
-
-    for (int i = 0; i < 6; i++) actions[i].update();
+    for (int i = 0; i < sizeof(actions)/sizeof(actions[0]); i++)
+      actions[i].update();
+    this_thread::sleep_for(20);
   }
 }
